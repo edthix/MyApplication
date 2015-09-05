34f4d7fab160819bb104fcdaf5359e0e2c998dfb
Edham Arief / edthix <edthix@gmail.com>
2015-09-06 03:51:08 +0800

    Setting Up the Action Bar

--- app/src/main/java/com/example/ed/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ed/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ed.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -22,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //        return true;
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -31,14 +36,28 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //        if (id == R.id.action_settings) {
+        //            return true;
+        //        }
+        //
+        //        return super.onOptionsItemSelected(item);
 
-        return super.onOptionsItemSelected(item);
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                //openSearch();
+
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /** When user clicks send button **/
